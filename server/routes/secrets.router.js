b9d8c27075f77e085e21df462ba11bb378599f1b
David Vinson <davidvinson018@gmail.com>
2021-06-02 14:27:13 -0500

    user level query displays correct info

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
 
+
+  let clearance_level = req.user.clearance_level;
+  let queryText = `
+    SELECT * FROM "secret" WHERE "secrecy_level" <= $1;`;
+
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(queryText, [clearance_level])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
@@ -16,3 +25,12 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
+
+/*
+// Handles Ajax request for user information if user is authenticated
+router.get('/', rejectUnauthenticated, (req, res) => {
+  // Send back user object from the session (previously queried from the database
+  res.send(req.user);
+});
+
+*/
